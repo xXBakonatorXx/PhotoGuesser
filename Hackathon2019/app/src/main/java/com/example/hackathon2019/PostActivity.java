5dec269ddf095649c30288807ee811972da81be6
Brannon Chan <32146556+xXBakonatorXx@users.noreply.github.com>
2019-02-17 10:55:34 -0500

    Add files via upload

---
+++ Hackathon2019/app/src/main/java/com/example/hackathon2019/PostActivity.java
@@ -0,0 +1,14 @@
+package com.example.hackathon2019;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+
+}
